5ce9ebd1f438dbd4891c2be55829d025b59cecef
chcloudcode <light_fighting@163.com>
2019-06-19 18:07:27 +0800

    update project

--- src/main/java/com/server/concurrency/ConcurrencyConfig.java
+++ src/main/java/com/server/concurrency/ConcurrencyConfig.java
@@ -1,30 +1,15 @@
 package com.server.concurrency;
 
-
 import com.server.concurrency.examples.ThreadLocal.MyFilter;
 import com.server.concurrency.examples.ThreadLocal.MyInterceptor;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@RestController
-@SpringBootApplication
-public class ConcurrencyDemoApplication extends WebMvcConfigurerAdapter {
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(ConcurrencyDemoApplication.class, args);
-    }
-
-    @GetMapping("/test")
-    public void test(){
-
-    }
+@Component
+public class ConcurrencyConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public FilterRegistrationBean httpFilter(){

0d050836c39ee91fb58fd4b0f9301d390e241a5c
chcloudcode <light_fighting@163.com>
2019-12-03 11:20:24 +0800

    update BinarySearchTree

--- src/main/java/com/server/DataStructure/Tree/BinarySearchTree.java
+++ src/main/java/com/server/DataStructure/Tree/BinarySearchTree.java
@@ -66,8 +66,35 @@ public class BinarySearchTree extends TraverseTree{
             if(p.data>target) p=p.rightNode;
             else p=p.leftNode;
         }
+        //没有找到要删除的节点就退出
+        if(p==null) return;
 
+        //遍历结束后，p节点就是 要删除的节点
 
+        //要删除的节点，有两个子节点，则需要找到右子树中的最小节点，然后替换要删除的节点
+        if(p.leftNode!=null&&p.rightNode!=null){
+            TreeNode rightTreeNode = p.rightNode;
+            TreeNode rightTreeNodeParent = rightTreeNode;
+            //找 右子树中的 最小 节点
+            while (rightTreeNode.leftNode!=null){
+                rightTreeNodeParent=rightTreeNode;
+                rightTreeNode=rightTreeNode.leftNode;
+            }
+            // 遍历结束后 rightTreeNode 就是 右子树最小节点，rightTreeNodeParent 是最小节点的父节点
+            p.data= rightTreeNode.data;
+            p = rightTreeNode;
+            parent= rightTreeNodeParent;
+        }
+
+        //删除的节点是叶子节点或者只有一个子节点
+        TreeNode child ;
+        if(p.leftNode!=null) child=p.leftNode;
+        else if(p.rightNode!=null) child=p.rightNode;
+        else child=null;
+
+        if (parent == null) root = child; // 删除的是根节点
+        else if (parent.leftNode == p) parent.leftNode = child;
+        else parent.rightNode = child;
 
     }
 
